1fb6c4afae1e642979c75f9c5df958b0ca3cc7d4
Stefano Paulo <stefanopaulo47@gmail.com>
2020-09-26 16:12:46 -0300

    filter

---
+++ filter1.js
@@ -0,0 +1,12 @@
+const produtos = [
+    { nome: 'Notebook', preco: 2499, fragil: true },
+    { nome: 'iPad Pro', preco: 4199, fragil: true },
+    { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
+    { nome: 'Copo de Plástico', preco: 18.99, fragil: false }
+]
+
+const caro = produto => produto.preco >= 500
+const fragil = produto => produto.fragil
+const resultado = produtos.filter(caro).filter(fragil)
+
+console.log(resultado)
